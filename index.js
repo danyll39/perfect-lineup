11f0dd95395c22ace8acfc302cbdd124efa6b749
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-10-31 17:44:04 -0400

    Function addded to check for number of duplicate game ids and number of duplicate team ids

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 function validateLineup(lineup) {
-  console.log(lineup)
   const totalSalaries = lineup.reduce((agg, element) => {
     return element.salary + agg
   }, 0)
-
   const OFPosition = lineup.reduce(function (agg, element) {
     return agg + (element.position === 'OF')
   }, 0)
   // lineup.positions must include ['C', 'P', 'SS', '1B', '2B', '3B']
+  const gameDuplicates = (lineup) => lineup.reduce((countWords, word) => {
+    countWords[word.gameId] = ++countWords[word.gameId] || 1
 
+    return countWords
+  }, [])
+  const games = gameDuplicates(lineup).reduce(function (a, b) {
+    return Math.max(a, b)
+  })
+  const teamDuplicates = (lineup) => lineup.reduce((countWords, word) => {
+    countWords[word.teamId] = ++countWords[word.teamId] || 1
 
-  // lineup.teamId   <= 2 from each
+    return countWords
+  }, [])
+  const teams = teamDuplicates(lineup).reduce(function (a, b) {
+    return Math.max(a, b)
+  })
 
-
-  // lineup. gameId  <= 3 from each
-
-
-
-
-  return totalSalaries <= 45000 && OFPosition === 3
+  return totalSalaries <= 45000 && OFPosition === 3 && games <= 3 && teams <= 2
 }
+
 
 
 
