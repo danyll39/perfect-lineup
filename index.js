f76425696e887cd857d01f3b0cb17354f7eb1dd6
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-11-01 16:57:18 -0500

    All test passing

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ function validateLineup(lineup) {
   const OFPosition = lineup.reduce(function (agg, element) {
     return agg + (element.position === 'OF')
   }, 0)
-  // lineup.positions must include ['C', 'P', 'SS', '1B', '2B', '3B']
+  const numberOfPlayers = lineup.reduce(function (agg, element) {
+    return agg + (element.position !== 'OF')
+  }, 0)
   const gameDuplicates = (lineup) => lineup.reduce((countWords, word) => {
     countWords[word.gameId] = ++countWords[word.gameId] || 1
 
@@ -22,9 +24,18 @@ function validateLineup(lineup) {
   const teams = teamDuplicates(lineup).reduce(function (a, b) {
     return Math.max(a, b)
   })
+  const positionDuplicates = (lineup) => lineup.reduce((countWords, word) => {
+    countWords[word.position] = ++countWords[word.position] || 1
 
-  return totalSalaries <= 45000 && OFPosition === 3 && games <= 3 && teams <= 2
+    return countWords
+  }, [])
+  const oneOfEachPosition = Object.keys(positionDuplicates(lineup))
+
+  return totalSalaries <= 45000 && OFPosition === 3 && games <= 3 && teams <= 2 &&
+oneOfEachPosition.includes('P' && '3B' && '1B' && 'SS' && '2B' && 'C') && numberOfPlayers === 6
 }
+
+
 
 
 
